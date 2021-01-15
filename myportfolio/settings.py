4c77ba4b60f1c2a615ee7761f33b9f96e06e2c5e
Robert Marineau <robert.marineau@ivanhoecambridge.com>
2021-01-14 19:02:05 -0500

    no whitenoise now

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,4 +123,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+##STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
