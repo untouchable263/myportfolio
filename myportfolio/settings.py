22e4f2d877fdfe7c046e380b95a3a8fb974d429f
Robert Marineau <robert.marineau@ivanhoecambridge.com>
2021-01-16 12:41:56 -0500

    test url

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = STATIC_ROOT = 'D:/home/site/wwwroot/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
